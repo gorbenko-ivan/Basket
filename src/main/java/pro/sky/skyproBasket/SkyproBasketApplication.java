d49afd36316c184ed1916fe6254a84471ae758c3
gorbenko-ivan <Puzo05@mail.ru>
2025-03-05 09:54:55 +0300

    initial commit

---
+++ src/main/java/pro/sky/skyproBasket/SkyproBasketApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyproBasket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproBasketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproBasketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/skyproBasket/SkyproBasketApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyproBasket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproBasketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
